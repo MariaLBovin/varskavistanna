c0fb26e540428b79a65546b9e585d81118e7039e
MariaLBovin <maria.bovin@gmail.com>
2024-06-18 15:36:13 +0200

    Button component added

---
+++ plugnrate.client/src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+
+import { IButtonProps } from '../interfaces'
+import './button.css';
+
+const Button = ({variant, text, onClick}:IButtonProps) => {
+  const buttonClass = `button ${variant === 'primary' ? 'button-primary' : 'button-secondary'}`;
+
+  return (
+    <button className={buttonClass} onClick={onClick}>{text}</button>
+  )
+}
+
+export default Button
