9cd843fa2159f362ecb8c8a5c0488f84e39609d1
MariaLBovin <maria.bovin@gmail.com>
2024-07-03 09:44:22 +0200

    inputfield tsx added

---
+++ plugnrate.client/src/components/Input/Input.tsx
@@ -0,0 +1,31 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { IInputProps } from "./interface"
+import IconSearch from "../../assets/icons/IconSearch";
+import './input.css';
+
+const Input = ({placeholder, onSubmit}: IInputProps) => {
+    const [address, setAddress] = useState<string>('')
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setAddress(event.target.value)
+    }
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        onSubmit(address);
+        
+        
+      };
+    
+    return (
+    <form onSubmit={handleSubmit} className="input-container">
+        <div className="input-container-wrapper">
+        <input placeholder={placeholder} onChange={handleChange} className="input-container-field"></input>
+        <button type='submit' className="input-container-button">
+            <IconSearch/>
+        </button>
+        </div>
+        
+    </form>
+  )
+}
+
+export default Input
